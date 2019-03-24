a3230279f2e53305a93de318cd7a4cef9097aed7
bartcoelus <bart@coelus.net>
2019-03-24 21:34:29 +0100

    Update cookey.sol

--- cookey.sol
+++ cookey.sol
@@ -44,6 +44,50 @@ contract Cookey {
         address addr;
         uint numFiles;
         mapping (uint => file) files;
-    }
+        }
     
     mapping(uint=>file) files;
+
+    // uint vaultID;
+    mapping (uint => Vault) Vaults;
+
+   function newVault() public returns (uint vaultID) 
+   {
+        vaultID = numVaults++; // vaultID is return variable
+        // Creates new struct and saves in storage. We leave out the mapping type.
+        //Vault[vaultID] = Vault(addr, 0);
+        return vaultID;
+    }
+    
+    function storefile(string memory _IPFShash, string memory _FileTopic, address _beneficiary) public
+        {
+        fileID++;
+        files[fileID].IPFShash=_IPFShash;
+        files[fileID].FileTopic=_FileTopic;
+        files[fileID].sender=msg.sender;
+        files[fileID].beneficiary=_beneficiary;
+        
+        }
+        
+    function getfile(uint _fileID) public view returns(string memory IPFShash, string memory FileTopic,address  sender,  address  beneficiary)
+    {
+        //if (msg.sender = files[_fileID].sender) 
+        return (files[_fileID].IPFShash, files[_fileID].FileTopic,files[_fileID].sender, files[_fileID].beneficiary);
+        //return files[_fileID].sender;
+        //return files[_fileID].beneficiary;
+    }
+    
+    function getMyFiles (uint _fileID) public view returns (string memory)
+    {
+        if (files[_fileID].sender == msg.sender)
+     {return (files[_fileID].IPFShash);
+         
+     }
+         else
+        {}
+    }
+    function getIPFSfileTopic(uint vaultID) public view returns(string memory)
+    {
+        return files[vaultID].FileTopic;
+    }
+}
