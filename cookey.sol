aa9ebe615e1ec121b07acef87226fac436500794
bartcoelus <bart@coelus.net>
2019-03-24 20:43:59 +0100

    Update cookey.sol

--- cookey.sol
+++ cookey.sol
@@ -1,1 +1,26 @@
+pragma solidity ^0.5.1;
+    
+// 
+// Fortune Cookey says: Good things happen to those who wait
+//
+//              __
+//             / _)         
+//      .-^^^-/ /          
+//   __/       /              
+//  <__.|_|-|_|              
+//
 
+contract Cookey {
+
+//declare variables to use in the Cookey smart contract
+  address owner;
+  uint VaultID;
+  uint numVaults;
+  uint fileID;
+
+//constructor - this function runs on initialization (creation) of the smart contract
+    constructor() public {
+        owner = msg.sender;
+        numVaults = 1;
+        fileID = 1;
+    }
